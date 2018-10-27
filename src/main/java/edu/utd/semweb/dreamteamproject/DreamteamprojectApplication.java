46feede2568558adbd3f96d8a13f0b10d3724379
Unknown <antonrogo@gmail.com>
2018-10-27 14:33:35 -0500

    Added working sample REST template query

--- src/main/java/edu/utd/semweb/dreamteamproject/DreamteamprojectApplication.java
+++ src/main/java/edu/utd/semweb/dreamteamproject/DreamteamprojectApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URLEncoder;
 
 @SpringBootApplication
 public class DreamteamprojectApplication implements ApplicationRunner {
@@ -23,9 +29,27 @@ public class DreamteamprojectApplication implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         System.out.print("Test");
 
-       csv2RdfService.generateRdfFromCsvAirQuality();
-       csv2RdfService.generateRdfFromCsvChronicDiseases();
+       //csv2RdfService.generateRdfFromCsvAirQuality();
+       //csv2RdfService.generateRdfFromCsvChronicDiseases();
+
+
         jenaService.createFusekiServer();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:3330/rdf")
+                .queryParam("query",
+                        "PREFIX ut: <http://utdallas/semclass#> select * where { ?s ut:statename \"Texas\" }");
+
+        URI uri = builder.build().toUri();
+
+        ResponseEntity<Object> response
+                = restTemplate.getForEntity(uri  , Object.class);
+
+
+        System.out.println("Test");
+
 
     }
 }
